60f9ef88a7f3aa08df614b4606ce1ba0c36b7ced
parjfy <shabdansyrga15@gmail.com>
2026-02-27 20:08:45 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import service.CafeSystem;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        CafeSystem system = new CafeSystem();
+        system.run();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Тест: забронируем первый столик вручную (потом сделаем меню)
+        // system.tables.get(0).setBooked(true);
+        // system.saveData();
+
+        // Запусти 2 раза — второй раз должно загрузить данные из файла
     }
 }
